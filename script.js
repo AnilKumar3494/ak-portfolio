3331974c3645bb7abe2e63355ad99e6cf74253bb
Anil Kumar <90452951+AnilKumar3494@users.noreply.github.com>
2023-04-23 11:54:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,17 +9,3 @@ let nav_listEl = document.querySelector("#nav_list");
 function asideNavBar() {
   nav_listEl.classList.toggle("nav_transform");
 }
-
-let nav_itemEL = document.querySelector("#nav_item");
-function selected() {
-  nav_itemEL.classList.toggle("active");
-}
-
-const activePage = window.location.pathname;
-const navLinksEl = document
-  .querySelectorAll(".aside .nav li a")
-  .forEach((link) => {
-    if (link.href.includes(`${activePage}`)) {
-      link.classList.add("active");
-    }
-  });
