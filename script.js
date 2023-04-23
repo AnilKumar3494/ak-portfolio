84ad4cf51fd1693242913a0a647825614e60df81
Anil Kumar <90452951+AnilKumar3494@users.noreply.github.com>
2023-04-23 16:19:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ let nav_listEl = document.querySelector("#nav_list");
 function asideNavBar() {
   nav_listEl.classList.toggle("nav_transform");
 }
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 300, fill: "forwards" }
+  );
+};
